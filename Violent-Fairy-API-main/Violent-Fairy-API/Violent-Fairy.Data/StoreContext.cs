4d6134731ecb36d056785642c08d1be5350b76e7
“Cate <catherine.sladoje@gmail.comgit config --global user.email catherine.sladoje@gmail.com>
2022-04-12 14:50:11 -0400

    Create Database Context

---
+++ Violent-Fairy-API-main/Violent-Fairy-API/Violent-Fairy.Data/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using Violent.Fairy.Domain.Catalog;
+using Microsoft.EntityFrameworkCore;
+
+namespace Violent_Fairy.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        { }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
+
