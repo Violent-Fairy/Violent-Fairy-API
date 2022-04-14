313856713df1120506b07dea0594dd414cf84e04
“Cate <catherine.sladoje@gmail.comgit config --global user.email catherine.sladoje@gmail.com>
2022-04-14 12:27:35 -0400

    Adding Startup Data

--- Violent-Fairy-API-main/Violent-Fairy-API/Violent.Fairy.Data/StoreContext.cs
+++ Violent-Fairy-API-main/Violent-Fairy-API/Violent.Fairy.Data/StoreContext.cs
@@ -9,6 +9,12 @@ namespace Violent_Fairy.Data
         { }
 
         public DbSet<Item> Items { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            DbInitializer.Initialize(builder);
+        }
     }
 }
 
