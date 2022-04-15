bb1be6b0b686b83753e680ad25ce68a6ceacd8a0
“Cate <catherine.sladoje@gmail.comgit config --global user.email catherine.sladoje@gmail.com>
2022-04-15 02:23:40 -0400

    Generating Orders Controller

--- Violent-Fairy-API-main/Violent-Fairy-API/Violent.Fairy.Data/StoreContext.cs
+++ Violent-Fairy-API-main/Violent-Fairy-API/Violent.Fairy.Data/StoreContext.cs
@@ -9,6 +9,7 @@ namespace Violent.Fairy.Data
         { }
 
         public DbSet<Item> Items { get; set; }
+        public DbSet<Order> Orders { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
---
+++ Violent-Fairy-API-main/Violent-Fairy-API/Violent.Fairy.Domain/Catalog/OrderItem.cs
@@ -0,0 +1,12 @@
+using Violent.Fairy.Domain.Catalog;
+
+namespace Violent.Fairy.Domain.Orders
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public Item Item { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price => Item.Price * Quantity;
+    }
+}
