12eb1d3d5997578b4410a518a63f5782e96c4f37
Nick Pirro <2000ntp@gmail.com>
2022-04-18 16:32:25 -0400

    testing

--- Violent-Fairy-API/tests/Violent-Fairy.Domain.Tests/RatingTests.cs
+++ Violent-Fairy-API/tests/Violent-Fairy.Domain.Tests/RatingTests.cs
@@ -1,12 +1,24 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
 namespace Violent_Fairy.Domain.Tests;
 
 [TestClass]
-public class UnitTest1
+public class RatingTests
 {
     [TestMethod]
-    public void TestMethod1()
+    public void Can_Create_New_rating()
     {
+        var rating = new Rating(1, "Mike", "Great fit!");
+
+        Assert.AreEqual(1,rating.Stars);
+        Assert.AreEqual("Mike",rating.UserName);
+        Assert.AreEqual("Great fit!", rating.Review);
     }
+
+    [TestMethod]
+    [ExpectedException(typeof(argumentException))]
+    public void Cannot_Create_Rating_With_Invalid_Stars(){
+        var rating = new RatingTests(0, "Mike", "Great fit!");
+
+    }
+
 }
