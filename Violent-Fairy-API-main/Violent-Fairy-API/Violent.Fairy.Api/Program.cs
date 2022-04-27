44943db3c3dd3090e65f1426b1193dd01a9c5dc8
Joel Mathew <mathew.145@buckeyemail.osu.edu>
2022-04-27 13:44:48 -0400

    configure api

--- Violent-Fairy-API-main/Violent-Fairy-API/Violent.Fairy.Api/Program.cs
+++ Violent-Fairy-API-main/Violent-Fairy-API/Violent.Fairy.Api/Program.cs
@@ -12,6 +12,8 @@ string authority = builder.Configuration["Auth0:Authority"] ??
 string audience = builder.Configuration["Auth0:Audience"] ??
      throw new ArgumentNullException("Auth0:Audience");
 
+string storeConnectionString = builder.Configuration.GetConnectionString("StoreConnection") ??
+     throw new ArgumentNullException("ConnectionString:StoreConnection");
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -34,7 +36,7 @@ builder.Services.AddAuthorization(options =>
 
     });
 builder.Services.AddDbContext<StoreContext>(options => 
-    options.UseSqlite("Data Source=../Registrar.sqlite", 
+    options.UseSqlServer(storeConnectionString, 
     b => b.MigrationsAssembly("Violent.Fairy.Api"))
     );
 builder.Services.AddCors(options =>
